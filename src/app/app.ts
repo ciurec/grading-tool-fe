d704b0f07dec1d046e29df09ef72b096b2e44821
ciurec <Ciurean.Catalin@outlook.com>
2025-08-20 22:51:43 +0300

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatTable} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTable],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
